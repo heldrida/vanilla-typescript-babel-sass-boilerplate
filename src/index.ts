f4c4f7d7044c46d7773efe3c34726f8cbe1dbef5
Helder Oliveira <heldrida@gmail.com>
2020-07-07 16:15:55 +0100

    a quick look into rollup to see how it compares to webpack

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { sayHello } from './test'
+
+function HelloWorld(message: string): string {
+  return sayHello(message)
+}
+
+export default HelloWorld
---
+++ src/test.ts
@@ -0,0 +1,7 @@
+const sayHello = (message: string): string => {
+  return message
+}
+
+export {
+  sayHello
+}
