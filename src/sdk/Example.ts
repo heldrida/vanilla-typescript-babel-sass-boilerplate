47aed0774212c3bd76d4c3e6dc05f6aa63614801
Helder Oliveira <heldrida@gmail.com>
2020-07-09 12:31:11 +0100

    add logo to initial load screen

--- src/sdk/Example.ts
+++ src/sdk/Example.ts
@@ -1,9 +1,17 @@
 const createNode = () => {
+  // const body: HTMLBodyElement | null = document.querySelector('body')
+  // const h1 = document.createElement('h1')
+  // const text = document.createTextNode('Hello World!')
+  // h1.appendChild(text)
+  // body?.appendChild(h1)
+
   const body: HTMLBodyElement | null = document.querySelector('body')
-  const h1 = document.createElement('h1')
-  const text = document.createTextNode('Hello World!')
-  h1.appendChild(text)
-  body?.appendChild(h1)
+  const img: HTMLImageElement | null = document.createElement('img')
+
+  if (img) {
+    img.src = 'https://raw.githubusercontent.com/heldrida/vanilla-typescript-babel-sass-boilerplate/master/static/images/logo.png'
+    body?.appendChild(img)
+  }
 }
 
 export default createNode
